70684ebc71cbcb39f755d64c0e6693ec9c463f46
Nate Hobi <nate.hobi@gmail.com>
2016-04-12 10:45:53 -0400

    Initial commit

---
+++ includes/class-leo-quiz-results-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://github.com/gr33k01
+ * @since      1.0.0
+ *
+ * @package    Leo_Quiz_Results
+ * @subpackage Leo_Quiz_Results/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Leo_Quiz_Results
+ * @subpackage Leo_Quiz_Results/includes
+ * @author     Nate Hobi <nate.hobi@gmail.com>
+ */
+class Leo_Quiz_Results_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-leo-quiz-results-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://github.com/gr33k01
+ * @since      1.0.0
+ *
+ * @package    Leo_Quiz_Results
+ * @subpackage Leo_Quiz_Results/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Leo_Quiz_Results
+ * @subpackage Leo_Quiz_Results/includes
+ * @author     Nate Hobi <nate.hobi@gmail.com>
+ */
+class Leo_Quiz_Results_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
